dd8e0a499f8c87037b9186a66979e10c2a679990
Benjamin Armintor <armintor@gmail.com>
2015-04-22 09:04:58 -0400

    regenerate dummy app

--- lib/dbla/engine.rb
+++ lib/dbla/engine.rb
@@ -1,5 +1,9 @@
 module Dbla
   class Engine < ::Rails::Engine
     isolate_namespace Dbla
+    config.generators do |g|
+      g.test_framework :rspec
+      g.fixture_replacement :factory_girl, :dir => 'spec/factories'
+    end
   end
 end
